aa4f305d3f15dbc452413c1cfa7638497edf10aa
karolbulwin <karolbulwin@gmail.com>
2020-11-08 15:55:34 +0100

    feat: update url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+const API_URL = 'https://karolbulwin.github.io/hangman/';
+// const API_URL = 'http://localhost:4200/';
+
 export const environment = {
-  production: true
+  production: true,
+  API_URL
 };
